e7f4af9e18db4504497ea9d0d665777c64c9d236
Chalat Rahul <2006764@kfintech.com>
2024-01-18 11:54:19 +0530

    gh-pages

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function About() {
     return (
         <div className=" h-[100vh] bg-no-repeat bg-cover w-[100%] bg-[url('')]">
-<h1 className='text-4xl text-black absolute flex items-center justify-center h-[100%] w-[100%] left-0 top-0'>About</h1>
+<h1 className='text-4xl text-black  absolute flex items-center justify-center h-[100%] w-[100%] left-0 top-0'>About</h1>
             </div>
 
             )
